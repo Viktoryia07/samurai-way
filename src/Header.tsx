c2223a819d5e4163645baf0c1f850a280efd0e6c
Viktoryia07 <viktoriyarybik@gmail.com>
2022-11-01 10:52:10 +0300

    Commit 1

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header () {
+    return <div>
+            <a href='#s'>Home </a>
+            <a href='#s'>News Feed </a>
+            <a href='#s'>Messages</a>
+        </div>
+}
